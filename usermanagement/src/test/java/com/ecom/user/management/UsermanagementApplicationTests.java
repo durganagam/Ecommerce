14ed3a6dd7d9c3ebfb255743b81efca9f5150fe2
madhukar.pendyala <madhukar.pendyala@blujaysolutions.com>
2020-08-24 20:06:12 +0530

    Template for user management

---
+++ usermanagement/src/test/java/com/ecom/user/management/UsermanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecom.user.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsermanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
