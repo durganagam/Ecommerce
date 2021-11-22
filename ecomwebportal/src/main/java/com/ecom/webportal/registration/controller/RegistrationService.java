ae347afdb5f75478471c419be0ba8dcd3dcff605
durga <durganagam.javadev@gmail.com>
2021-11-22 16:20:39 +0530

    Rename

--- ecomwebportal/src/main/java/com/ecom/webportal/registration/controller/RegistrationService.java
+++ ecomwebportal/src/main/java/com/ecom/webportal/registration/controller/RegistrationService.java
@@ -13,7 +13,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 import com.ecom.webportal.registration.entity.Name;
 import com.ecom.webportal.registration.entity.OtpVerificationRequest;
 import com.ecom.webportal.registration.entity.User;
-import com.ecom.webportal.registration.entity.UserSignInRequest;
+import com.ecom.webportal.registration.entity.UserSignInRequest1;
 import com.ecom.webportal.registration.entity.UserSignUpRequest;
 
 import io.netty.handler.codec.http.multipart.Attribute;
@@ -22,7 +22,7 @@ import io.netty.handler.codec.http.multipart.Attribute;
 public class RegistrationService {
 
 	/**
-	 * Method is responsible to add {@link UserSignInRequest} attribute to model. so
+	 * Method is responsible to add {@link UserSignInRequest1} attribute to model. so
 	 * added {@link Attribute} further used by thymeleaf template engine to bind
 	 * form data.
 	 * 
@@ -35,12 +35,12 @@ public class RegistrationService {
 	}
 
 	/**
-	 * Method is responsible to add {@link UserSignInRequest} attribute to model.
+	 * Method is responsible to add {@link UserSignInRequest1} attribute to model.
 	 * 
 	 * @param model {@link Model}
 	 */
 	public void signIn(final Model model) {
-		UserSignInRequest userSignInRequest = new UserSignInRequest();
+		UserSignInRequest1 userSignInRequest = new UserSignInRequest1();
 		userSignInRequest.setUserId("");
 		userSignInRequest.setOtp("");
 		model.addAttribute("userSignInRequest", userSignInRequest);
@@ -59,7 +59,7 @@ public class RegistrationService {
 	public String createUser(final UserSignUpRequest userSignUpRequest, Model model, HttpSession session) {
 		String user = callUserMgmtServiceToCreateUser(userSignUpRequest);
 		if (Objects.nonNull(user)) {
-			UserSignInRequest userSignInRequest = new UserSignInRequest();
+			UserSignInRequest1 userSignInRequest = new UserSignInRequest1();
 			//userSignInRequest.setMobileNo(user.getMobileNo());
 			userSignInRequest.setUserId(user);
 			userSignInRequest.setOtp("");
@@ -70,7 +70,7 @@ public class RegistrationService {
 		return "signin";
 	}
 
-	public String login(UserSignInRequest userSignInRequest, Model model, HttpSession session) {
+	public String login(UserSignInRequest1 userSignInRequest, Model model, HttpSession session) {
 		User user = callUserMgmtServiceToLoginUser(userSignInRequest);
 		if (user != null) {
 			session.setAttribute("userId", user.getUserId());
@@ -83,12 +83,12 @@ public class RegistrationService {
 	 * Method is responsible to verify the user OTP. It internally call user
 	 * management service to validate the entered otp.
 	 * 
-	 * @param userSignInRequest {@link UserSignInRequest}
+	 * @param userSignInRequest {@link UserSignInRequest1}
 	 * @param model             {@link Model}
 	 * @param session           {@link HttpSession}
 	 * @return home page if successful else remains on the same with error message.
 	 */
-	public String verifyOtp(final UserSignInRequest userSignInRequest, final Model model, final HttpSession session) {
+	public String verifyOtp(final UserSignInRequest1 userSignInRequest, final Model model, final HttpSession session) {
 		final OtpVerificationRequest otpVerificationRequest = new OtpVerificationRequest();
 		otpVerificationRequest.setOtp(userSignInRequest.getOtp());
 		otpVerificationRequest.setUserId((String) session.getAttribute("userId"));
@@ -117,7 +117,7 @@ public class RegistrationService {
 				.bodyToMono(String.class).block();
 	}
 
-	private User callUserMgmtServiceToLoginUser(final UserSignInRequest userSignInRequest) {
+	private User callUserMgmtServiceToLoginUser(final UserSignInRequest1 userSignInRequest) {
 		return WebClient.create("http://localhost:8080/users/login").post().bodyValue(userSignInRequest.getMobileNo())
 				.header("website", "ecom").accept(MediaType.APPLICATION_JSON).retrieve()
 				.onStatus(httpStatus -> !HttpStatus.OK.equals(httpStatus),
