c2aa0d3916b27bc0c9c13ca00e5182dad429ccc4
durga <durga@DESKTOP-S79987I>
2021-11-22 16:09:21 +0530

    rename

---
+++ ecomwebportal/src/main/java/com/ecom/webportal/registration/entity/UserSignInRequest1.java
@@ -0,0 +1,33 @@
+package com.ecom.webportal.registration.entity;
+
+public class UserSignInRequest1 {
+
+	private String userId;
+	private String otp;
+	private String mobileNo;
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getOtp() {
+		return otp;
+	}
+
+	public void setOtp(String otp) {
+		this.otp = otp;
+	}
+
+	public String getMobileNo() {
+		return mobileNo;
+	}
+
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+
+}
